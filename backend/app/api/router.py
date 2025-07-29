c5f39f1a7dd1b96fabc17146edf3dcbbc3459f6d
Yuri Kuniyoshi <e235750@ie.u-ryukyu.ac.jp>
2025-07-30 00:02:44 +0900

    ブックマーク処理に伴うapi作成

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from .endpoints import words, wordbooks
+from .endpoints import words, wordbooks, bookmarks
 
 api_router = APIRouter()
 
 api_router.include_router(words.router, prefix="/words", tags=["words"])
 api_router.include_router(wordbooks.router, prefix="/wordbooks", tags=["wordbooks"])
+api_router.include_router(bookmarks.router, prefix="/bookmarks", tags=["bookmarks"])
